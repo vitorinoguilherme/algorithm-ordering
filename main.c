98ed80fad2a665c7f8d06bc0139acf17d56b1e68
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-03-27 15:56:24 -0300

    Criado programa principal teste

---
+++ main.c
@@ -0,0 +1,35 @@
+/* 	main.c - Conômetro tempo de ordenação da lista de valores.
+	autor: Guilherme Vitorino
+	20.03.20 - 21h25min */
+	
+#include <stdio.h>
+
+#define ROW 10
+#define COLUMN 10
+
+////int main(int argc, char *argv[]) {
+//	menuLayout();
+//}
+
+void menuLayout() {
+	 
+	printf("\n\n\t1. Bubble Sort"	
+		"\n\t2. Insertion Sort"
+		"\n\t3. Selection Sort"
+		"\n\t4. Merge Sort"
+		"\n\t5. Quick Sort"
+		"\n\t6. Radix Sort"
+		"\n\t7. Heap Sort");
+	
+}
+
+void displayArrayList() {
+	int * pointerArrayList;
+	int arrayList[10];
+	
+	FILE * filePointer;
+	
+	//filePointer = fopen();
+	
+	//while()
+}
